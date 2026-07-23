070d6f7dc01fd710bacc61636c702a888a2a02b6
FlintCox <1020557@schoolsnet.act.edu.au>
2026-07-23 10:44:38 +1000

    it turns!

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,24 @@ vehicle myCar;
 
 #define leftLED 2
 #define rightLED 12
-
+#define Anticlockwise Contrarotate
+/*
+myCar.Move(Move_Left, 255);
+myCar.Move(Move_Right, 255);
+myCar.Move(Forward, 255);
+myCar.Move(Backward, 255);
+myCar.Move(Anticlockwise, 255);
+myCar.Move(Clockwise, 255);
+myCar.Move(Stop, 0);
+*/
 void setup()
 {
  Serial.begin(115200);
  pinMode(rightLED, OUTPUT);
  pinMode(leftLED, OUTPUT);
+
+ myCar.Init();
+
 }
 
 void loop()
@@ -20,6 +32,12 @@ void loop()
   // put your main code here, to run repeatedly:
 digitalWrite(leftLED, HIGH);
 digitalWrite(rightLED, LOW);
+myCar.Move(Move_Left, 255);
+delay(1000);
+digitalWrite(rightLED, HIGH);
+digitalWrite(leftLED, LOW);
+myCar.Move(Move_Right, 255);
+delay(1000);
 
 delay(100);
   
