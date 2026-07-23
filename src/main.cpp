8d91f4e2804323d89ee963e4669550800140a5a4
FlintCox <1020557@schoolsnet.act.edu.au>
2026-07-23 10:04:12 +1000

    first save

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,22 @@
 
 vehicle myCar;
 
+#define leftLED 2
+#define rightLED 12
 
 void setup()
 {
- 
+ Serial.begin(115200);
+ pinMode(rightLED, OUTPUT);
+ pinMode(leftLED, OUTPUT);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
+digitalWrite(leftLED, HIGH);
+digitalWrite(rightLED, LOW);
 
+delay(100);
   
 }
